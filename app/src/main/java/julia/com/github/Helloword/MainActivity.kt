e891561190f2c5aa6e94f58026a8c9be6e13e39e
analugz <analuisagiaquinto@gmail.com>
2025-03-05 10:56:14 -0300

    refatorando main activity para modificar text ao digitar

--- app/src/main/java/julia/com/github/Helloword/MainActivity.kt
+++ app/src/main/java/julia/com/github/Helloword/MainActivity.kt
@@ -1,7 +1,7 @@
 package julia.com.github.Helloword
 
 import android.os.Bundle
-import android.widget.Button
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity  //Usamos a AppCompatActivity
@@ -14,21 +14,24 @@ class MainActivity : AppCompatActivity() {  //A herança é de AppCompatActivity
         // Aqui é onde associamos a Activity ao layout XML
         setContentView(R.layout.activity_main)  // Usamos setContentView para associar o layout XML
 
-        // Inicializando os componentes da interface
-        val etName = findViewById<EditText>(R.id.etName)  // EditText para digitar o nome
-        val tvHelloMessage = findViewById<TextView>(R.id.tvHelloMessage)  // TextView para mostrar a mensagem
-        val btnUpdateMessage = findViewById<Button>(R.id.btnUpdateMessage)  // Botão para atualizar a mensagem
+        //Acessando componentes do arquivo XML usando o findViewById
+        //Todos os componentes são views
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvHelloMessage = findViewById<TextView>(R.id.tvHelloMessage)
 
-        // Definindo o comportamento do botão
-        btnUpdateMessage.setOnClickListener {
-            // Pegando o texto que o usuário digitou no EditText
-            val name = etName.text.toString()
-
-            // Atualizando o TextView com a mensagem desejada
-            if (name.isNotBlank()) {
-                tvHelloMessage.text = "Hello, $name!"  // Se o nome não estiver vazio, exibe "Hello, {nome}"
+        // Listener para quando o usuário pressiona Enter (actionDone)
+        etName.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val name = textView.text.toString().trim()
+                // Verifica se o nome foi digitado
+                if (name.isNotEmpty()) {
+                    tvHelloMessage.text = "Hello World, $name!"
+                } else {
+                    tvHelloMessage.text = "Hello World!"
+                }
+                true // Indica que consumimos o evento
             } else {
-                tvHelloMessage.text = "Hello World!"  // Caso o nome esteja vazio, exibe "Hello World!"
+                false
             }
         }
     }
